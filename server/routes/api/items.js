95c958454a1f73873fcf334758681b4c208eb3bb
rutenis <rutis98@gmail.com>
2019-11-08 18:21:37 +0000

    added slice method to the api

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -6,15 +6,27 @@ const items = require('../../data/item_list');
 router.get('/', (req, res) => res.json(items));
 
 // Get Single item
-router.get('/:id', (req, res) => {
-  const found = items.some(item => item.id === parseInt(req.params.id));
+// router.get('/:id', (req, res) => {
+//   const found = items.some(item => item.id === parseInt(req.params.id));
+
+//   if (found) {
+//     res.json(items.filter(item => item.id === parseInt(req.params.id)));
+//   } else {
+//     res.status(400).json({ msg: `No item with the id of ${req.params.id}` });
+//   }
+// });
+// Paginate Items
+router.get('/p/:start/:end', (req, res) => {
+
+  const start = parseInt(req.params.start);
+  const end = parseInt(req.params.end);
+  const found = items.slice(start, end);
 
   if (found) {
-    res.json(items.filter(item => item.id === parseInt(req.params.id)));
+    res.json(items.slice(start, end));
   } else {
     res.status(400).json({ msg: `No item with the id of ${req.params.id}` });
   }
 });
-
 
 module.exports = router;
