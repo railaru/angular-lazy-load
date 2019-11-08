201e853acc519c856b441032c8a20471898a6f47
rutenis <rutis98@gmail.com>
2019-11-08 18:33:57 +0000

    api get single item

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -5,6 +5,17 @@ const items = require('../../data/item_list');
 // Get All items
 router.get('/', (req, res) => res.json(items));
 
+// Get Single item
+router.get('/:id', (req, res) => {
+  const found = items.some(item => item.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(items.filter(item => item.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No item with the id of ${req.params.id}` });
+  }
+});
+
 // Paginate Items
 router.get('/p/:start/:end', (req, res) => {
 
@@ -15,7 +26,7 @@ router.get('/p/:start/:end', (req, res) => {
   if (found) {
     res.json(found);
   } else {
-    res.status(400).json({ msg: `No item with the id of ${req.params.id}` });
+    res.status(400).json({ msg: `No items with the specified parameters` });
   }
 });
 
