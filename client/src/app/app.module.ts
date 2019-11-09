7d0fa5088b347f8f2c7dd9486a76c3b0c33567d4
rutenis <rutis98@gmail.com>
2019-11-09 17:25:44 +0000

    add card component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ApiService } from './services/api.service';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
