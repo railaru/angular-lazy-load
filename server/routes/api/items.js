a4077c38eea964559a33e189c3b9603f21ae7cda
rutenis <rutis98@gmail.com>
2019-11-10 20:24:47 +0000

    remove get single item function in items.js

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -5,17 +5,6 @@ const items = require('../../data/item_list');
 // Get All items
 router.get('/', (req, res) => res.json(items));
 
-// Get Single item
-router.get('/:id', (req, res) => {
-  const found = items.some(item => item.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(items.filter(item => item.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No item with the id of ${req.params.id}` });
-  }
-});
-
 // Paginate Items
 router.get('/page/:page_number/amount/:page_amount', (req, res) => {
   const page = parseInt(req.params.page_number) - 1;
